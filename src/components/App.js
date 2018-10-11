370782e3c2038a707bf52118d45748789a51859b
mikepcarlos <michaelpcarlos@gmail.com>
2018-10-11 15:19:50 -0400

    my branch

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,13 @@ import Nav from './Nav'
 import hogs from '../porkers_data';
 
 class App extends Component {
+
   render() {
+    console.log(hogs)
     return (
       <div className="App">
           < Nav />
+          < HogBrowser allHogs={getHogs}/>
 
       </div>
     )
---
+++ src/components/HogBrowser.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HogBrowser extends Component {
+
+
+  render() {
+    return(
+      
+    )
+  }
+}
